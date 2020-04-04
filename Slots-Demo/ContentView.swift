fa8a7095587d87fc76f367b540905ac65bd314dd
Murillo R. Araujo <mr_araujo@icloud.com>
2020-04-03 21:07:02 -0300

    Add spin function

--- Slots-Demo/ContentView.swift
+++ Slots-Demo/ContentView.swift
@@ -9,8 +9,103 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    private var symbols = ["apple", "star", "cherry"]
+    private var betAmount = 5
+    
+    @State private var numbers = [0, 2, 0]
+    @State private var credits = 1000
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Rectangle()
+                .foregroundColor(Color(red: 200/255, green: 143/255, blue: 32/255))
+                .edgesIgnoringSafeArea(.all)
+            
+            Rectangle()
+                .foregroundColor(Color(red: 228/255, green: 195/255, blue: 76/255))
+                .rotationEffect(Angle(degrees: 45))
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Spacer()
+                
+                HStack {
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.yellow)
+                    
+                    Text("SwftUI Slots")
+                        .bold()
+                        .foregroundColor(.white)
+                    
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.yellow)
+                    
+                } .scaleEffect(2)
+                
+                Spacer()
+                
+                Text("Credits: " + String(self.credits))
+                    .foregroundColor(.black)
+                    .padding(.all, 10)
+                    .background(Color.white.opacity(0.5))
+                    .cornerRadius(20)
+                
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    
+                    Image(symbols[numbers[0]])
+                        .resizable()
+                        .aspectRatio(1, contentMode: .fit)
+                        .padding()
+                        .background(Color.white.opacity(0.5))
+                        .cornerRadius(20)
+                        .scaleEffect(0.9)
+                    
+                    Image(symbols[numbers[1]])
+                        .resizable()
+                        .aspectRatio(1, contentMode: .fit)
+                        .padding()
+                        .background(Color.white.opacity(0.5))
+                        .cornerRadius(20)
+                        .scaleEffect(0.9)
+                    
+                    Image(symbols[numbers[2]])
+                        .resizable()
+                        .aspectRatio(1, contentMode: .fit)
+                        .padding()
+                        .background(Color.white.opacity(0.5))
+                        .cornerRadius(20)
+                        .scaleEffect(0.9)
+                    
+                    Spacer()
+                }
+                Spacer()
+                
+                Button(action: {
+                    self.numbers[0] = Int.random(in: 0...self.symbols.count - 1)
+                    self.numbers[1] = Int.random(in: 0...self.symbols.count - 1)
+                    self.numbers[2] = Int.random(in: 0...self.symbols.count - 1)
+                    
+                    if self.numbers[0] == self.numbers[1] && self.numbers[1] == self.numbers[2] {
+                        self.credits += self.betAmount * 5
+                    } else {
+                        self.credits -= self.betAmount
+                    }
+                }) {
+                    Text("Spin")
+                        .bold()
+                        .foregroundColor(.white)
+                        .padding(.all, 10)
+                        .padding([.leading, .trailing], 30)
+                        .background(Color.pink)
+                        .cornerRadius(20)
+                }
+                Spacer()
+            }
+        }
     }
 }
 
